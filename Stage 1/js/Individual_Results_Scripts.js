4268707713050f7325cabad0732536ebbf22a42a
JohnnyCrusher <John.huynh@student.qut.edu.au>
2018-03-21 01:22:05 +1000

    fixed some small errors and added some functionality on hover rating system

---
+++ Stage 1/js/Individual_Results_Scripts.js
@@ -0,0 +1,37 @@
+function ratingHover(item){
+    var icons = item.firstChild;
+    var itemId = icons.getAttribute("id");
+
+    var oneStar = document.getElementById('one-star');
+    var twoStar = document.getElementById('two-star');
+    var threeStar = document.getElementById('three-star');
+    var fourStar = document.getElementById('four-star');
+    var fiveStar = document.getElementById('five-star');
+
+    var stars = [oneStar,twoStar,threeStar,fourStar,fiveStar];
+    var number = 0;
+    for(var i = 0; i<stars.length;i++){
+        if(itemId == (stars[i].getAttribute("id"))){
+            number = i + 1;
+        }
+    }
+    for(var i = 0; i<number; i++){
+        stars[i].classList.remove("fa-star-o");
+        stars[i].classList.add("fa-star");
+    }
+}
+function ratingHoverOut(item){
+    console.log("activated")
+    var oneStar = document.getElementById('one-star');
+    var twoStar = document.getElementById('two-star');
+    var threeStar = document.getElementById('three-star');
+    var fourStar = document.getElementById('four-star');
+    var fiveStar = document.getElementById('five-star');
+
+    var stars = [oneStar,twoStar,threeStar,fourStar,fiveStar];
+
+    for(var i = 0; i<stars.legnth; i++){
+        stars[i].classList.remove("fa-star");
+        stars[i].classList.add("fa-star-o");
+    }
+}
